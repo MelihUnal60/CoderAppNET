5dcda227691f3d168ab5b235e6d34bb94789e731
MelihUnal60 <111892158+MelihUnal60@users.noreply.github.com>
2023-03-11 00:42:08 +0300

    Add project files.

---
+++ AppNET.Domain/Entities/Cash.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Cash
+{
+    public decimal Price { get; set; }
+
+    public DateTime OperationDate { get; set; }
+
+    public string Description { get; set; }
+
+
+}
