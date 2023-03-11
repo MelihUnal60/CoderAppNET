3f922798947017522c4f2e7904796858491fbe51
MelihUnal60 <111892158+MelihUnal60@users.noreply.github.com>
2023-03-11 10:16:43 +0300

    Shopping service

---
+++ AppNET.App/ShoppingService.cs
@@ -0,0 +1,43 @@
+﻿using AppNET.Domain.Entities;
+using AppNET.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppNET.App
+{
+    public class ShoppingService
+    {
+        private readonly IRepository<Product> _productsRepository;
+
+        public void SellProduct(string name,int stock,decimal price)
+        {
+            Product soldProduct = new Product();
+            soldProduct.Name = name;
+            soldProduct.Price = price;
+            soldProduct.Stock = stock;
+            if (ControlStockForSale(name, stock))
+                _productsRepository.Remove(soldProduct);
+            else
+                throw new Exception("Satış işlemi gerçekleştirilemedi!!");
+
+        }
+
+        public bool ControlStockForSale(string saledProduct,int sellAmount)
+        {
+            Product productForSale = new Product();
+            
+            productForSale = _productsRepository.GetList().FirstOrDefault(p => p.Name== saledProduct);
+
+            if (sellAmount < productForSale.Stock)
+            {
+                throw new Exception("En fazla" + " " + productForSale.Stock + " " + "ürün satılabilir!!");
+                return false;
+            }
+            else
+                return true;
+        }
+    }
+}
